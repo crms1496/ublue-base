5d6e7179e7d90d7fbe79ed91dc020f2ccaa5cea5
crms1496 <crms1496@gmail.com>
2024-05-03 19:27:43 -0400

    Update build.sh

--- build.sh
+++ build.sh
@@ -15,8 +15,6 @@ curl -Lo /etc/yum.repos.d/tailscale.repo https://pkgs.tailscale.com/stable/fedor
 # List of rpmfusion packages can be found here:
 # https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/39/x86_64/repoview/index.html&protocol=https&redirect=1
 
-
 rpm-ostree install clevis-dracut clevis-luks clevis-udisks2 clevis-systemd \
     byobu duperemove fish mosh podman-compose tailscale \
     cockpit cockpit-machines cockpit-networkmanager cockpit-ostree cockpit-pcp cockpit-podman cockpit-selinux cockpit-storaged cockpit-systemd
-
